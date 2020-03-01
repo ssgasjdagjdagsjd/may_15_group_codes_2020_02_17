88693e9a2acc97aa2e074782c94b809bf09c93f3
Yusif Alizada <yusifedu@gmail.com>
2020-03-01 14:58:34 +0400

    basket

--- year2/angular/todolist/src/app/component/basket/basket.component.ts
+++ year2/angular/todolist/src/app/component/basket/basket.component.ts
@@ -8,20 +8,23 @@ import { BasketProduct } from 'src/app/model/basket';
   styleUrls: ['./basket.component.css']
 })
 export class BasketComponent implements OnInit {
-basketProducts:BasketProduct[]=[];
-  constructor(private bS:BasketService) { }
+//basketProducts:BasketProduct[]=[];
+  constructor(public bS:BasketService) { }
 
   ngOnInit() {
-    this.basketProducts=this.bS.basketProducts.slice();
+    //this.basketProducts=this.bS.basketProducts.slice();
 
   }
 getTotalPrice(){
   let tP:number=0;
-  for (let index = 0; index < this.basketProducts.length; index++) {
-    const e = this.basketProducts[index];
+  for (let index = 0; index < this.bS.basketProducts.length; index++) {
+    const e = this.bS.basketProducts[index];
     tP+=e.product.price*e.quantity;
     
   }
   return tP;
+}
+onDeleteProduct(counter:number){
+  this.bS.basketProducts.splice(counter,1);
 }
 }
