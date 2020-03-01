be478e7e4731410e0f50e01027dc26b9adccf5dd
Yusif Alizada <yusifedu@gmail.com>
2020-03-01 14:59:04 +0400

    product basket

--- year2/angular/todolist/src/app/component/products/products.component.ts
+++ year2/angular/todolist/src/app/component/products/products.component.ts
@@ -12,22 +12,51 @@ import { BasketComponent } from '../basket/basket.component';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  products:Product[]=[];
-  constructor(private productService:ProductService,private basketService:BasketService,private matD:MatDialog) { }
+  products: Product[] = [];
+  constructor(private productService: ProductService, private basketService: BasketService, private matD: MatDialog) { }
 
   ngOnInit() {
-    this.products=this.productService.products.slice();
-    
-  }
-  onToBasket(p:Product){
+    this.products = this.productService.products.slice();
 
-    let basketProduct:BasketProduct=new  BasketProduct();
-    basketProduct.quantity=1;
-    basketProduct.product=p;
-   this.basketService.basketProducts.push(basketProduct);
+  }
+  onToBasket(p: Product) {
+
+    let basketProduct: BasketProduct = new BasketProduct();
+    basketProduct.quantity = 1;
+    basketProduct.product = p;
+    this.basketService.basketProducts.push(basketProduct);
+
+    this.showToBasketButton = false;
+    setTimeout(() => {
+      this.showToBasketButton = true;
+    }, 200);
+
+
   }
 
-  onOpenBasket(){
-this.matD.open(BasketComponent);
+  onOpenBasket() {
+    this.matD.open(BasketComponent);
+
+  }
+
+  trimText(text: string, length: number) {
+    if (text.length > length) {
+      return text.substr(0, length) + '...';
+    } else {
+      return text;
+    }
+  }
+
+
+  showToBasketButton: boolean = true;
+
+  findAllProductQuantity(): number {
+    let count: number = 0;
+    for (let index = 0; index < this.basketService.basketProducts.length; index++) {
+      const e = this.basketService.basketProducts[index];
+      count += e.quantity;
+    }
+
+    return count;
   }
 }
