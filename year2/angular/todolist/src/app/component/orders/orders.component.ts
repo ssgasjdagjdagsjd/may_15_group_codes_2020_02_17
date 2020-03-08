655c83c3ae5639604f17410c4ddb779c2c820501
Yusif Alizada <yusifedu@gmail.com>
2020-03-08 14:15:56 +0400

    print order total price

--- year2/angular/todolist/src/app/component/orders/orders.component.ts
+++ year2/angular/todolist/src/app/component/orders/orders.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderService } from 'src/app/service/order.service';
+import { OrderModel } from 'src/app/model/order';
 
 @Component({
   selector: 'app-orders',
@@ -13,5 +14,13 @@ export class OrdersComponent implements OnInit {
   ngOnInit() {
     this.oS.loadLastOrders();
   }
+  calculateOrderTotalPrice(o:OrderModel):number{
+let total:number=0;
 
+for (let index = 0; index < o.basketProducts.length; index++) {
+  const e = o.basketProducts[index];
+  total+=e.product.price*e.quantity;
+}
+return total;
+  }
 }
