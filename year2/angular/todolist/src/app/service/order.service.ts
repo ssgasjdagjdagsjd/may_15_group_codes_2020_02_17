0509944c21c58b04907d26b7c7167654739fd7d9
Yusif Alizada <yusifedu@gmail.com>
2020-03-15 14:23:37 +0400

    Update order.service.ts

--- year2/angular/todolist/src/app/service/order.service.ts
+++ year2/angular/todolist/src/app/service/order.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { OrderModel } from '../model/order';
+import { LoginService } from './login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
   orders: OrderModel[] = [];
-  constructor() {
+  constructor(private lS:LoginService) {
     this.loadLastOrders();
 
   }
@@ -14,9 +15,19 @@ export class OrderService {
 
     let ordersString: string = localStorage.getItem('orders');
 
-    if (ordersString) {
-      let orders: OrderModel[] = JSON.parse(ordersString);
-      this.orders = orders;
+    if (ordersString) { 
+      let orders:OrderModel[]  = JSON.parse(ordersString);  
+      let ordersTemp:OrderModel[]=[];
+
+for (let index = 0; index < orders.length; index++) {
+  const o = orders[index];
+  if(o.username===this.lS.username){
+    ordersTemp.push(o);
+  }
+}
+
+this.orders=ordersTemp;
+
     } else {
       
       let orders: OrderModel[]=[];
