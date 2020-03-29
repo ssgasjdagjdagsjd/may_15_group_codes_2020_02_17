e14be13892338c4d624d8c8ca0ea619e6adcd184
Yusif Alizada <yusifedu@gmail.com>
2020-03-29 13:49:26 +0400

    student register

---
+++ year2/backend/JSP_Servlet_May15/WebContent/student-list.jsp
@@ -0,0 +1,48 @@
+<%@page import="az.developia.student.model.Student"%>
+<%@page import="java.util.ArrayList"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script
+	src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<title>student list</title>
+</head>
+<body>
+
+	<table class="table table-bordered">
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Surname</th>
+			</tr>
+		</thead>
+		<tbody>
+			<%
+				ArrayList<Student> students = (ArrayList<Student>) request.getAttribute("students");
+				for (Student s : students) {
+					
+			%>
+
+			<tr>
+				<td><%=s.getName()%></td>
+				<td><%=s.getSurname()%></td>
+			</tr>
+
+			<%
+				}
+			%>
+		</tbody>
+	</table>
+
+</body>
+</html>
---
+++ year2/backend/JSP_Servlet_May15/src/az/developia/student/StudentController.java
@@ -0,0 +1,52 @@
+package az.developia.student;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import az.developia.student.model.Student;
+
+@WebServlet("/student-controller")
+public class StudentController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private ArrayList<Student> students=new ArrayList<>();
+	
+	
+	public StudentController() {
+		super();
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		
+		String name=request.getParameter("name");
+		String surname=request.getParameter("surname");
+		
+		Student s=new Student();
+		
+		s.setName(name);
+		s.setSurname(surname);
+		students.add(s);
+		request.setAttribute("students", students);
+		
+		RequestDispatcher dispatcher=request.getRequestDispatcher("student-list.jsp");
+		dispatcher.forward(request, response);
+		
+		
+		
+	}
+
+}
