4344982b1660a3f0275d1ec27b0900b17c129d0f
Yusif Alizada <yusifedu@gmail.com>
2020-04-12 14:53:00 +0400

    products test api created

---
+++ year2/backend/spring/shopping/src/main/java/az/developia/shopping/controller/ProductController.java
@@ -0,0 +1,47 @@
+package az.developia.shopping.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import az.developia.shopping.model.Product;
+
+@RestController
+@RequestMapping(path = "/products")
+
+public class ProductController {
+
+	@GetMapping(path = "find-one")
+	public Product getOneProduct() {
+		Product p = new Product();
+		p.setId(101);
+		p.setName("Apple");
+		p.setPrice(4.5D);
+		return p;
+	}
+
+	@GetMapping(path = "find-multiple")
+	public List<Product> getMultipleProduct() {
+		List<Product> products = new ArrayList<>();
+
+		Product p1 = new Product();
+		p1.setId(101);
+		p1.setName("Apple");
+		p1.setPrice(4.5D);
+
+		products.add(p1);
+		
+		Product p2 = new Product();
+		p2.setId(102);
+		p2.setName("Orange");
+		p2.setPrice(6.5D);
+
+		products.add(p2);
+
+		return products;
+	}
+
+}
