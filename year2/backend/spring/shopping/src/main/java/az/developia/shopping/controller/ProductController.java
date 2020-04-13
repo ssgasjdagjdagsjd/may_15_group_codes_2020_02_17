7d218947b86f947dcd9e3aa005c42ad21edd0276
Yusif Alizada <yusifedu@gmail.com>
2020-04-13 20:57:16 +0400

    Update ProductController.java
    put, delete api test

--- year2/backend/spring/shopping/src/main/java/az/developia/shopping/controller/ProductController.java
+++ year2/backend/spring/shopping/src/main/java/az/developia/shopping/controller/ProductController.java
@@ -1,15 +1,13 @@
 package az.developia.shopping.controller;
 
-import java.sql.Connection;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,44 +19,40 @@ import az.developia.shopping.model.Product;
 @RequestMapping(path = "/products")
 
 public class ProductController {
-
-	@GetMapping(path = "find-one")
-	public Product getOneProduct() {
-		Product p = new Product();
-		p.setId(101);
-		p.setName("Apple");
-		p.setPrice(4.5D);
-		return p;
-	}
-
-	@GetMapping(path = "find-multiple")
-	public List<Product> getMultipleProduct() {
-		List<Product> products = new ArrayList<>();
-
-		Product p1 = new Product();
-		p1.setId(101);
-		p1.setName("Apple");
-		p1.setPrice(4.5D);
-
-		products.add(p1);
-		
-		Product p2 = new Product();
-		p2.setId(102);
-		p2.setName("Orange");
-		p2.setPrice(6.5D);
-
-		products.add(p2);
-
-		return products;
-	}
-	
 	@Autowired
 	private ProductDAO productDAO;
 	
 	
+	
+	@GetMapping(path = "/{id}")  
+	public Product getOneProduct(@PathVariable(name="id") Integer id) {
+		 
+		return productDAO.findById(id).get();
+	}
+
+	@GetMapping
+	public List<Product> getMultipleProduct() { 
+		return productDAO.findAll();
+	}
+	
+	
+	
 @PostMapping
 public Product save(@RequestBody Product p){
+	p.setId(null); 
 	 productDAO.save(p);
 	return p;
+}
+@PutMapping
+public Product update(@RequestBody Product p){
+	 
+	 productDAO.save(p);
+	return p;
+}
+
+@DeleteMapping(path = "/{id}")  
+public void deleteById(@PathVariable(name="id") Integer id) {
+	 
+	  productDAO.deleteById(id);
 }
 }
