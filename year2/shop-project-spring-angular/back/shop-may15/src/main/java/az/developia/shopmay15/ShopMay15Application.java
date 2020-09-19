d05ed379bafb181aa85958f6d22889fb04721987
Yusif Alizada <yusifedu@gmail.com>
2020-09-19 13:21:15 +0400

    shop back created
    bu addimda shop layihesi ucun yaratdigimiz back proyektini əlavə etdik

---
+++ year2/shop-project-spring-angular/back/shop-may15/src/main/java/az/developia/shopmay15/ShopMay15Application.java
@@ -0,0 +1,13 @@
+package az.developia.shopmay15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMay15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopMay15Application.class, args);
+	}
+
+}
---
+++ year2/shop-project-spring-angular/back/shop-may15/src/test/java/az/developia/shopmay15/ShopMay15ApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.shopmay15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMay15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
